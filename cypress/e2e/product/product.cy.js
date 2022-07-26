59fa8ebec76da63fecd8b82f585c17c675ea18b2
Niels Kop <niels.kop@sawiday.com>
2022-07-26 14:46:29 +0200

    asdfasd

--- cypress/e2e/product/product.cy.js
+++ cypress/e2e/product/product.cy.js
@@ -1,14 +1,14 @@
 describe('The Product Page', () => {
-    it('successfully loads', () => {
+    it('contains all necessary elements', () => {
         // visit
-        cy.visit('https://www.sanitairwinkel.nl/p/76907250/adema-circle-badkamerspiegel-rond-diameter-60cm-met-indirecte-led-verlichting-met-spiegelverwarming-en-touch-schakelaar');
-        //
-        // // check if bought-together-products is visible
-        // cy.get('#bought-together-products')
-        //     .should('be.visible');
-        //
-        // // check if same-serie-products is visible
-        // cy.get('#same-serie-products')
-        //     .should('be.visible');
+        cy.visit('/p/76907250/');
+
+        // check if bought-together-products is visible
+        cy.get('#bought-together-products')
+            .should('be.visible');
+
+        // check if same-serie-products is visible
+        cy.get('#same-serie-products')
+            .should('be.visible');
     })
 })
