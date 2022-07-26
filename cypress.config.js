b70f3fa62638cf2de021178d86348a306bbf86e5
Niels Kop <niels.kop@sawiday.com>
2022-07-26 14:23:17 +0200

    asdf

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    video: false, // disable recording video for cypress run
+    e2e: {
+        baseUrl: 'https://local.sanitairwinkel.nl',
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        blockHosts: [
+            '*.hotjar.com',
+            '*.datatrics.com',
+            '*.google-analytics.com',
+            '*.googletagmanager.com'
+        ],
+    },
+});
