d5a9dbd81f2e00643fbb08cc048fd63819116a92
Niels Kop <niels.kop@sawiday.com>
2022-07-26 14:26:51 +0200

    prod host

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
     video: false, // disable recording video for cypress run
     e2e: {
-        baseUrl: 'https://172.31.75.118:3000',
+        baseUrl: 'https://sanitairwinkel.nl',
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
