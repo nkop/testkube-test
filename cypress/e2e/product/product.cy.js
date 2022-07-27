9f1576f3ade83be869ee81a3440c47cfb98f4a94
Niels Kop <niels.kop@sawiday.com>
2022-07-27 08:26:22 +0200

    log? '

--- cypress/e2e/product/product.cy.js
+++ cypress/e2e/product/product.cy.js
@@ -2,6 +2,7 @@ describe('The Product Page', () => {
     it('contains all necessary elements', () => {
         // visit
         cy.visit('https://sanitairwinkel.nl/p/76907250/');
+        cy.log(cy.url());
 
         // check if bought-together-products is visible
         cy.get('#bought-together-products')
